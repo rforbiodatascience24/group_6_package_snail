43c41766a9439d4aaf173b7a9577ef7da6b0ac4c
MagdaDz <s194428@student.dtu.dk>
2024-10-29 10:07:05 +0100

    create codon_table_snail2 object

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon table snail 2
+#'
+#' Here we try making a codon table
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table_snail2"
